09e485362374361dfae6d97d2d50467bfd8989a6
cmanea <cosmin.manea@ibm.com>
2025-02-04 18:47:59 +0200

    Stock

---
+++ script.py
@@ -0,0 +1,16 @@
+{
+    "directory": "C:\\Users\\Y9DU4P826\\Documents\\pers_projects\\stock",
+    "no_pictures": 6,
+    "db_hostname": "localhost",
+    "db_user": "jamemwiw_user",
+    "db_password": "]BRg_ma_vF=*",
+    "db": "jamemwiw_db",
+    "db_port": "5522",
+    "ssh_hostname": "199.188.200.150",
+    "ssh_user": "jamemwiw",
+    "ssh_password": "NQdQ1SQfMO2a",
+    "ssh_port": "21098",
+    "upload_path": "/home/jamemwiw/public_html/uploads/"
+    
+    
+}
